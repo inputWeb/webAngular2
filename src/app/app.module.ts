378791880c98dca664774b056047c64fa4b7ec1e
Andriy Haydychuk <ha.personal.91@gmail.com>
2019-02-06 01:07:47 +0100

    Modificaciones generales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 //Importar las rutas en el core de Angular
-import {routing, appRoutingProviders}  from './app.routing';
+import { routing, appRoutingProviders } from './app.routing';
 import { AppRoutingModule } from './app-routing-module';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
 import { materialsModule } from './materialsModule';
-import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
 import { ArticulosComponent } from './articulos/articulos.component';
 import { PaginatorComponent } from './paginator/paginator.component';
 import { SliderNavComponent } from './slider-nav/slider-nav.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     ErrorComponent,
     ArticulosComponent,
     PaginatorComponent,
-    SliderNavComponent
+    SliderNavComponent,
+    LoginComponent,
+    NavbarComponent
   ],
+
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {Usuario} from '../models_entity/usuario';
-import {UsuariosService} from '../services/usuarios.service';
+import { Usuario } from '../models_entity/usuario';
+import { UsuariosService } from '../services/usuarios.service';
 import swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-login',
@@ -9,18 +10,21 @@ import swal from 'sweetalert2';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  titulo: string = "Login"
 
-  titulo:string = "Login"
-  private usuario: Usuario = new Usuario(); 
+  private usuario: Usuario = new Usuario();
 
   constructor(private usuarioService: UsuariosService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
 
-  create():void{
+  }
+
+  create(): void {
     this.usuarioService.create(this.usuario)
       .subscribe(json => {
         swal('Nuevo Usuario Registrado!', `${json.mensaje}: ${json.usuario.username}`, 'success');
       })
   }
+
 }
